5b2fbb47f29568edd4f1107fe5a170eea94daaff
a2un <encodedgeek@gmail.com>
2020-05-24 00:07:28 -0400

    fix bugs

--- cpp/examples/parquet/low-level-api/page-index-bloom-filter-test.sh
+++ cpp/examples/parquet/low-level-api/page-index-bloom-filter-test.sh
@@ -1,24 +1,23 @@
 $ARROW_HOME/build/debug/parquet-writer-with-pageindex  10000000
 
-mkdir -p member    &
-mkdir -p nonmember &
+mkdir -p $ARROW_HOME/build/debug/tests/nonmember    
+mkdir -p cd $ARROW_HOME/build/debug/tests/member 
 
-cp parquet_cpp_example_10000000_sorted.parquet member      &
-cp parquet_cpp_example_10000000_sorted.parquet nonmember   &
-cp parquet_cpp_example_10000000_unsorted.parquet member     &
-cp parquet_cpp_example_10000000_unsorted.parquet nonmember  &
+cp parquet_cpp_example_10000000_sorted.parquet member      
+cp parquet_cpp_example_10000000_sorted.parquet nonmember   
+cp parquet_cpp_example_10000000_unsorted.parquet member     
+cp parquet_cpp_example_10000000_unsorted.parquet nonmember 
 
 ## member queries
-cd member
-
+cd $ARROW_HOME/build/debug/tests/member
 $ARROW_HOME/build/debug/parquet-reader-with-pageindex parquet_cpp_example_10000000_sorted.parquet   10000000  >> debug_m_10Ms  &
 
 $ARROW_HOME/build/debug/parquet-reader-with-pageindex parquet_cpp_example_10000000_unsorted.parquet  10000000  >> debug_m_10Mu &
 
-cd ..
+
 
 ## non-member queries
-cd nonmember
+cd $ARROW_HOME/build/debug/tests/nonmember
 $ARROW_HOME/build/debug/parquet-reader-with-pageindex ../parquet_cpp_example_10000000_sorted.parquet   100000000  >> debug_n_10Ms &
 
 $ARROW_HOME/build/debug/parquet-reader-with-pageindex ../parquet_cpp_example_10000000_unsorted.parquet  100000000  >> debug_n_10Mu &
