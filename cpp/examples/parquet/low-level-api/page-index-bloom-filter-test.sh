fc0a475fd7dd239c17261c77d7a7b9edfbbbaab9
a2un <encodedgeek@gmail.com>
2020-05-23 23:32:31 -0400

    bg run

--- cpp/examples/parquet/low-level-api/page-index-bloom-filter-test.sh
+++ cpp/examples/parquet/low-level-api/page-index-bloom-filter-test.sh
@@ -1,12 +1,12 @@
 $ARROW_HOME/build/debug/parquet-writer-with-pageindex  10000000
 
-mkdir -p member
-mkdir -p nonmember
+mkdir -p member    &
+mkdir -p nonmember &
 
-cp parquet_cpp_example_10000000_sorted.parquet member
-cp parquet_cpp_example_10000000_sorted.parquet nonmember
-cp parquet_cpp_example_10000000_unsorted.parquet member
-cp parquet_cpp_example_10000000_unsorted.parquet nonmember
+cp parquet_cpp_example_10000000_sorted.parquet member      &
+cp parquet_cpp_example_10000000_sorted.parquet nonmember   &
+cp parquet_cpp_example_10000000_unsorted.parquet member     &
+cp parquet_cpp_example_10000000_unsorted.parquet nonmember  &
 
 ## member queries
 cd member
