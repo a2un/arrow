fe03f6fa9f579acd8daef956f21acc0ed602d199
a2un <encodedgeek@gmail.com>
2020-06-01 00:57:19 -0400

    perf command

--- cpp/examples/parquet/low-level-api/page-index-reader-test.sh
+++ cpp/examples/parquet/low-level-api/page-index-reader-test.sh
@@ -9,3 +9,7 @@ echo "launching non-member queries.."
 $ARROW_HOME/build/debug/parquet-reader-with-pageindex ~/parquet_data/parquet_cpp_example_10000000_sorted.parquet  10000000  >> $ARROW_HOME/build/debug/tests/nonmember/debug_n_10M_s  &
 
 $ARROW_HOME/build/debug/parquet-reader-with-pageindex ~/parquet_data/parquet_cpp_example_10000000_unsorted.parquet 10000000 >> $ARROW_HOME/build/debug/tests/nonmember/debug_n_10M_u &
+
+#perf record -ag -e faults -p $pid
+
+#iostat -k 1 -p sda > ~/parquet_data/debug_read_writes
